6e13e55c7e952713aa2ddf278b710cd6887e20d3
Bảo Ngọc <89443501+ngoc15025@users.noreply.github.com>
2026-03-31 21:42:57 +0700

    API đăng nhập

---
+++ BackEnd/DiemDanhLopHoc/DiemDanhLopHoc/DTOs/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DiemDanhLopHoc.DTOs
+{
+    public class LoginRequestDto
+    {
+        public string TaiKhoan { get; set; } = null!;
+        public string MatKhau { get; set; } = null!;
+    }
+}
