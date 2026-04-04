d6dccdec89e87b438daf4421d7b58a3a7f397d66
Bảo Ngọc <89443501+ngoc15025@users.noreply.github.com>
2026-04-04 22:21:10 +0700

    Delete BackEnd/DiemDanhLopHoc directory

--- BackEnd/DiemDanhLopHoc/DiemDanhLopHoc/DTOs/LoginRequestDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DiemDanhLopHoc.DTOs
-{
-    public class LoginRequestDto
-    {
-        public string TaiKhoan { get; set; } = null!;
-        public string MatKhau { get; set; } = null!;
-    }
-}
